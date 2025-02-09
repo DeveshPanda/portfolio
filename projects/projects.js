20f7e243b51ab468768e51925641d484930137d9
Devesh Panda <deveshpanda@Deveshs-MacBook-Pro.local>
2025-02-08 18:52:03 -0800

    added render pie function

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
@@ -6,4 +8,43 @@ renderProjects(projects, projectsContainer, 'h2');
 const projectsTitle = document.querySelector('.projects-title');
 if (projectsTitle) {
     projectsTitle.textContent = `${projects.length} Projects`;
+}
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+let selectedIndex = -1; // Track the current selection
+
+function renderPieChart(filteredProjects) {
+    let svg = d3.select('svg');
+    svg.selectAll('path').remove();
+    d3.select('.legend').selectAll('*').remove();
+
+    let rolledData = d3.rollups(
+        filteredProjects,
+        (v) => v.length,
+        (d) => d.year
+    );
+
+    let data = rolledData.map(([year, count]) => {
+        return { value: count, label: year };
+    });
+
+    let sliceGenerator = d3.pie().value((d) => d.value);
+    let arcData = sliceGenerator(data);
+    let arcs = arcData.map((d) => arcGenerator(d));
+
+    arcs.forEach((arc, idx) => {
+        svg.append('path')
+            .attr('d', arc)
+            .attr('fill', colors(idx))
+            .on('click', () => handleArcClick(idx, data));
+    });
+
+    let legend = d3.select('.legend');
+    data.forEach((d, idx) => {
+        legend.append('li')
+            .attr('style', `--color:${colors(idx)}`)
+            .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+    });
 }
